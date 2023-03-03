84e6dadc413dc81ea2407508a931f4c05628996b
Kelvin <13521005@mahasiswa.itb.ac.id>
2023-03-03 14:19:02 +0700

    preparation

---
+++ src/Maze.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Maze
+{
+    private readonly char[,] _maze;
+    private readonly int _size;
+
+    public Maze(int size)
+    {
+        _size = size;
+        _maze = new char[size, size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                _maze[i, j] = 'x';
+            }
+        }
+    }
+
+    public void createMaze(string input)
+    {
+        string text = File.ReadAllText("../doc/" + input +".txt");
+
+        string[] rows = text.Split('\n');
+        
+
+        for (int i = 0; i < rows.Length; i++)
+        {
+            for (int j = 0; j < rows[0].Length; j++)
+            {
+                _maze[i, j] = rows[i][j];
+            }
+        }
+
+        }
+    
+
+    public void PrintMaze()
+{
+
+    Console.WriteLine("Bentuk Maze: ");
+    Console.WriteLine();
+
+    for (int i = 0; i < _size; i++)
+    {
+        for (int j = 0; j < _size; j++)
+        {
+            Console.Write(_maze[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+[STAThread]
+static void Main(string[] args)
+{
+    Maze maze = new Maze(4);
+    Console.Write("Masukkan nama file: ");
+    string input = Console.ReadLine();
+    Console.WriteLine();
+    maze.createMaze(input);
+    maze.PrintMaze();
+}
+}
